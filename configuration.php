c35cd1f59651adbf5b5eb6fdf0acf5cae5e37a43
Davide Bizzarri <davide.bizzarri@wuerth-phoenix.com>
2018-01-18 11:42:58 +0100

    Create a menu entry foreach group in configuration  and updating config.example.ini

--- configuration.php
+++ configuration.php
@@ -5,10 +5,13 @@ use Icinga\Application\Config;
 /** @var \Icinga\Application\Modules\Module $this */
 $section = $this->menuSection(N_('Overview'));
 
-$menuName = Config::module('virtualgroups')->get('main', 'menu_name', 'Virtual Groups');
+$menuName = Config::module('virtualgroups')->get('main', 'menu_name', '%s Overview');
 $menuPriority = Config::module('virtualgroups')->get('main', 'menu_priority', 100);
+$virtualGroups = Config::module('virtualgroups')->getSection('groups')->toArray();
 
-$section->add($menuName, array(
-    'url'      => 'virtualgroups',
-    'priority' => $menuPriority
-));
+foreach ($virtualGroups as $key => $name) {
+    $section->add(sprintf($menuName, $name), array(
+        'url'      => sprintf('virtualgroups?group=%s', $key),
+        'priority' => $menuPriority
+    ));
+}
