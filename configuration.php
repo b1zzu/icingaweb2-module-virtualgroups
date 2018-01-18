4f91f7ae808d97bd76196b6aca342260dec8d865
Davide Bizzarri <davide.bizzarri@wuerth-phoenix.com>
2018-01-18 10:21:33 +0100

    Fixed default menu name

--- configuration.php
+++ configuration.php
@@ -5,7 +5,7 @@ use Icinga\Application\Config;
 /** @var \Icinga\Application\Modules\Module $this */
 $section = $this->menuSection(N_('Overview'));
 
-$menuName = Config::module('virtualgroups')->get('main', 'menu_name', 'Virtual Groups Hello');
+$menuName = Config::module('virtualgroups')->get('main', 'menu_name', 'Virtual Groups');
 $menuPriority = Config::module('virtualgroups')->get('main', 'menu_priority', 100);
 
 $section->add($menuName, array(
